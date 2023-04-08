3e1e234df68663f9fe3b883a80d082c2b5c5d25c
sedasenbol <ssenbol@gmail.com>
2023-04-08 21:23:08 +0300

    * Comment, User, Video and VideoStatus models were created.

---
+++ backend/src/main/java/com/senbol/seda/youtubeclone/model/Comment.java
@@ -0,0 +1,19 @@
+package com.senbol.seda.youtubeclone.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+    @Id
+    private String id;
+    private String text;
+    private String authorId;
+    private Integer likeCount;
+    private Integer dislikeCount;
+}
---
+++ backend/src/main/java/com/senbol/seda/youtubeclone/model/VideoStatus.java
@@ -0,0 +1,5 @@
+package com.senbol.seda.youtubeclone.model;
+
+public enum VideoStatus {
+    PUBLIC, PRIVATE, UNLISTED
+}
